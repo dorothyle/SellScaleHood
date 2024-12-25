21de7be5f20d28342b227a33c408e0e19167c08f
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-12-25 10:03:16 -0800

    create desktop view

--- sellscalehood-frontend/src/Homepage.tsx
+++ sellscalehood-frontend/src/Homepage.tsx
@@ -1,10 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import TradeMenu from "./TradeMenu.tsx";
 import Portfolio from "./Portfolio.tsx";
 import "./styling/Homepage.css";
 
 const Homepage = () => {
   const [displayTradeMenu, setDisplayTradeMenu] = useState<Boolean>(false);
+
+  useEffect(() => {
+    // Function to handle screen width check
+    const handleResize = () => {
+      if (window.innerWidth > 800) { // Set your threshold here, e.g., 768px
+        setDisplayTradeMenu(true);
+      } else {
+        setDisplayTradeMenu(false);
+      }
+    };
+
+    // Check screen width initially
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
   return (
     <div className="homepageContainer">
       <h1>Hi, Emma</h1>
