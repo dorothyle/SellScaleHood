881bbdffa569029cbab698951484b15814ee37c0
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-12-24 20:17:21 -0800

    direct app to different homepage

---
+++ sellscalehood-frontend/src/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchStock from './SearchStock.tsx';
+
+const Homepage = () => {
+  return (
+    <div>
+      <p>Hi, this is the homepage</p>
+      <SearchStock />
+    </div>
+  );
+}
+
+export default Homepage;
--- sellscalehood-frontend/src/index.tsx
+++ sellscalehood-frontend/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import Homepage from './Homepage.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Homepage />
   </React.StrictMode>
 );
 
